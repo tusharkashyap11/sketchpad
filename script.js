952c5c7c1f2d2e838107a966020ad95272b0a8ae
tusharkashyap11 <tushar.kashyap.tab@gmail.com>
2023-08-12 04:00:49 +0530

    Added rainbow marker feature and styled the app

--- script.js
+++ script.js
@@ -1,41 +1,97 @@
-let gridSize;
-let cellList;
-const setSize = document.getElementById('set-size');
 const container = document.getElementById('container');
+const setGrid = document.getElementById('set-grid');
+const grid = document.querySelector('.gridsize');
+const value = setGrid.value;
+let colorMode = 'black';
 
-setSize.addEventListener('click', () => {
+for (let i = 0; i < value*value; i++) {
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    div.style.width = String(500/value) + 'px';
+    div.style.height = String(500/value) + 'px';
     
+    container.appendChild(div);
+}
+
+let cellList = container.querySelectorAll('div');
+let isMouseDown = false;
+container.onclick = () => (isMouseDown = !isMouseDown);
+
+cellList.forEach (cell => {
+    cell.addEventListener('mouseover', () => {
+        if (isMouseDown) {
+            if (colorMode === 'rainbow') {
+                let color = getRandomColor();
+                cell.style.backgroundColor = color;
+            } else if (colorMode === 'black') {
+                cell.classList.add(colorMode);
+            }
+        }
+    })
+})
+
+const clear = document.getElementById('Clear');
+clear.addEventListener('click', function () {
+    for (const cell of cellList) {
+        if (colorMode === 'rainbow') {
+            cell.style.removeProperty('background-color');
+        } else {
+            cell.classList.remove(colorMode);
+        }
+    }
+})
+
+setGrid.addEventListener('input', () => {
+
     while (container.firstChild) {
         if (container.firstChild.tagName === 'DIV') {
           container.removeChild(container.firstChild);
         } else {
           container.firstChild.remove();
         }
-    }      
+    }     
 
-    gridSize = prompt("ENTER GRID SIZE");
-    while (gridSize < 0 && gridSize > 64 || gridSize == '') {
-        gridSize = prompt("ENTER VALID SIZE BETWEEN 0 AND 64");
-    }
+    const value = setGrid.value;
+    grid.textContent = value + " x " + value;
 
-    for (let i = 0; i < gridSize*gridSize; i++) {
+    for (let i = 0; i < value*value; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
-        div.style.width = String(600/gridSize ) + 'px';
-        div.style.height = String(600/gridSize ) + 'px';
-            
-        container.appendChild(div);
+        div.style.width = String(500/value) + 'px';
+        div.style.height = String(500/value) + 'px';
 
-        div.addEventListener('mouseover', () => {
-            div.classList.add('hovered')
-        });
+        container.appendChild(div);
     }
-    cellList = container.querySelectorAll('div');   
+    cellList = container.querySelectorAll('div');
+
+    cellList.forEach (cell => {
+        cell.addEventListener('mouseover', () => {
+            if (isMouseDown) {
+                if (colorMode === 'rainbow') {
+                    let color = getRandomColor();
+                    cell.style.backgroundColor = color;
+                } else if (colorMode === 'black') {
+                    cell.classList.add(colorMode);
+                }
+            }
+        })
+    })
 })
 
-const clear = document.getElementById('Clear');
-clear.addEventListener('click', function () {
-    for (const cell of cellList) {
-        cell.classList.remove('hovered');
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+}
+
+const rainbowMarker = document.getElementById('rainbow');
+rainbowMarker.addEventListener('click', () => {
+    colorMode = 'rainbow'; 
+})
+const blackMarker = document.getElementById('black');
+blackMarker.addEventListener('click', () => {
+    colorMode = 'black';  
 })
